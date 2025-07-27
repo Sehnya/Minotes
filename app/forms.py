7b23371edf9d061220414c265703eb373a95b67b
Sehnya <Sehnyaw@gmail.com>
2025-07-27 03:02:48 -0400

    update

--- app/forms.py
+++ app/forms.py
@@ -12,13 +12,15 @@ class MyForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
 
-    def validate_username(self, field):
+    @staticmethod
+    def validate_username(field):
         logger.info(f"Checking if username '{field.data}' already exists...")
         if User.get_or_none(User.username == field.data):
             logger.warning(f"Username '{field.data}' is already taken.")
             raise ValidationError('That username is taken. Please choose a different one.')
 
-    def validate_email(self, field):
+    @staticmethod
+    def validate_email(field):
         logger.info(f"Checking if email '{field.data}' already exists...")
         if User.get_or_none(User.email == field.data):
             logger.warning(f"Email '{field.data}' is already taken.")
